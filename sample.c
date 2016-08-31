a44582aa3a27610295169dba6dfd55a838d766af
YuviJohn <YuviJohn@users.noreply.github.com>
2016-08-31 12:27:05 +0530

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,1 +1,11 @@
-sdfsdfsdfsdfv
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  if (a=0)
+  {printf("The number is zero");}
+  if (a>0)
+  {printf("The number is positive");}
+  else{pintf("the number is negative");}
+}
